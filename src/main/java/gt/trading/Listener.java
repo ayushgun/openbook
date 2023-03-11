9c94f35131b85adf9734474f063b312e7d93761d
Giovanni Hsu <giovannihsu.123@gmail.com>
2023-03-10 20:01:05 -0500

    fetch depth data and printing

--- src/main/java/gt/trading/Listener.java
+++ src/main/java/gt/trading/Listener.java
@@ -2,8 +2,10 @@ package gt.trading;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONArray;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import okhttp3.Response;
 import okhttp3.WebSocket;
@@ -26,7 +28,8 @@ public class Listener extends WebSocketListener {
     System.out.println("WebSocket connection established");
     // webSocket.send("{\"sub\": \"market.ethbtc.kline.1min\",\"id\":
     // \"id1\"}");
-    webSocket.send("{\"sub\": \"market.btcusdt.ticker\"}");
+    // webSocket.send("{\"sub\": \"market.btcusdt.ticker\"}");
+    webSocket.send("{\"sub\": \"market.btcusdt.depth.step3\",\"id\": \"id1\"}");
   }
 
   /**
@@ -57,7 +60,30 @@ public class Listener extends WebSocketListener {
     if (jsonObject.containsKey("ping")) {
       webSocket.send("{\"pong\":" + jsonObject.get("ping") + "}");
     }
-    System.out.println("Received binary message: " + jsonObject);
+    // System.out.println("Received binary message: " + jsonObject);
+    if (jsonObject.containsKey("ch")) {
+      JSONObject depth = jsonObject.getJSONObject("tick");
+      JSONArray bids = (JSONArray) depth.get("bids");
+
+      System.out.println("-------------------");
+      System.out.println("Bids:");
+      for (int i = 0; i < Math.min(bids.size(), 5); i++) {
+        JSONArray order = (JSONArray) bids.get(i);
+        BigDecimal price = (BigDecimal) order.get(0);
+        BigDecimal quantity = (BigDecimal) order.get(1);
+        System.out.println(price + " --- " + quantity);
+      }
+      JSONArray asks = (JSONArray) depth.get("asks");
+      System.out.println("Asks:");
+      for (int i = 0; i < Math.min(asks.size(), 5); i++) {
+        JSONArray order = (JSONArray) asks.get(i);
+        BigDecimal price = (BigDecimal) order.get(0);
+        BigDecimal quantity = (BigDecimal) order.get(1);
+        System.out.println(price + " --- " + quantity);
+      }
+      System.out.println("-------------------");
+      // System.out.println(gg);
+    }
 
   }
 
