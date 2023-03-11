ab67362b57b3d7be80510fe9c8e949617627b034
Ayush <ayushgundawar@gmail.com>
2023-03-11 16:14:23 -0500

    📝 Added description

--- src/main/java/gt/trading/App.java
+++ src/main/java/gt/trading/App.java
@@ -1,7 +1,9 @@
 package gt.trading;
 
 public class App {
-    public static void main(String[] args) {
-        Events.connect();
-    }
+  public static void main(String[] args) {
+    // Initializes the web socket connection
+    // This is just test code for now.
+    Events.connect();
+  }
 }
