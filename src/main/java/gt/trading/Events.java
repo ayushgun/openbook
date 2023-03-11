2578a444cd438c0aefd8911beab4015cc01a4c1a
Ayush <ayushgundawar@gmail.com>
2023-03-11 16:14:14 -0500

    🗑 Remove instance variables

--- src/main/java/gt/trading/Events.java
+++ src/main/java/gt/trading/Events.java
@@ -4,13 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.WebSocket;
 
 /**
  * Provides methods to handle events from the Huobi API.
  */
 final class Events {
-  private static WebSocket webSocket;
 
   /**
    * Connect to the Huobi websocket API.
@@ -23,7 +21,7 @@ final class Events {
     Request request = new Request.Builder().url("wss://api.huobi.pro/ws")
         .build();
 
-    webSocket = client.newWebSocket(request, new Listener());
+    client.newWebSocket(request, new Listener());
 
     // Cleanly end the connection process
     client.dispatcher().executorService().shutdown();
