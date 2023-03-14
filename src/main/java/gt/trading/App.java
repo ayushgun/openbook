1d4dacaf6b5daead9986f359242fe2a0aa6006b8
Giovanni Hsu <giovannihsu.123@gmail.com>
2023-03-14 00:08:19 -0400

    cleanup

--- src/main/java/gt/trading/App.java
+++ src/main/java/gt/trading/App.java
@@ -1,11 +1,7 @@
 package gt.trading;
 
-import okhttp3.OkHttpClient;
-
 public class App {
   public static void main(String[] args) {
-    // Initializes the web socket connection
-    // This is just test code for now.
     OrderBook orderBook = new OrderBook();
   }
 }
