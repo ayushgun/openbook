0cf9e75f1a1b7d1f750eabb9afb9572a05e4228e
Ayush <ayushgundawar@gmail.com>
2023-04-05 22:41:51 -0400

    🧪 Added package information

---
+++ app/src/main/java/gt/trading/huobi/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package provides the core functionality for the Huobi order book
+ * project. It includes the main classes for handling events, data models, and
+ * listeners to process market data from various Huobi APIs, such as WebSocket
+ * events and REST APIs.
+ *
+ * The Huobi order book project aims to efficiently manage and process real-time
+ * market data to maintain an accurate representation of the order book for the
+ * BTC/USDT instrument.
+ *
+ * @author Georiga Tech Trading Club Team #2
+ * @version 1.0
+ * @since 1.0
+ */
+
+package gt.trading.huobi;
