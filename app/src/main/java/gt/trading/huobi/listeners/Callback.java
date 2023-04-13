2f209f1a22a10999debf33ce56ca88ac71e0cf51
Ayush <ayushgundawar@gmail.com>
2023-04-13 19:24:03 -0400

    📝 Documentation formatting

--- app/src/main/java/gt/trading/huobi/listeners/Callback.java
+++ app/src/main/java/gt/trading/huobi/listeners/Callback.java
@@ -4,7 +4,7 @@ package gt.trading.huobi.listeners;
  * Functional interface for defining a callback function to be executed when a
  * response is received.
  *
- * @param <T> The type of the response object.
+ * @param <T> the type of the response object
  */
 @FunctionalInterface
 public interface Callback<T> {
