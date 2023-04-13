6b770c788831bdb9403258fc1c4296ad56e33390
Ayush <ayushgundawar@gmail.com>
2023-04-13 19:35:50 -0400

    🗑 Remove reference to REST API

--- app/src/main/java/gt/trading/huobi/package-info.java
+++ app/src/main/java/gt/trading/huobi/package-info.java
@@ -1,8 +1,7 @@
 /**
  * This package provides the core functionality for the Huobi order book
  * project. It includes the main classes for handling events, data models, and
- * listeners to process market data from various Huobi APIs, such as WebSocket
- * events and REST APIs.
+ * listeners to process market data from various Huobi WebSocket events.
  *
  * The Huobi order book project aims to efficiently manage and process real-time
  * market data to maintain an accurate representation of the order book for the
