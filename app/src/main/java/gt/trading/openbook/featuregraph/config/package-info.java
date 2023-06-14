ed82d62301797eb0696adfa1a659364f85efee61
pranav-mani <pranav.manivannan@relevantz.com>
2023-06-14 14:39:03 -0400

    Updated package-info files

--- app/src/main/java/gt/trading/openbook/featuregraph/config/package-info.java
+++ app/src/main/java/gt/trading/openbook/featuregraph/config/package-info.java
@@ -1,5 +1,12 @@
 /**
- *
+ * This package contains the configuration class for the feature graph and the
+ * json file containing the builder path.
+ * 
+ * The FeatureGraphConfig class is responsible for retrieving the builder path
+ * stored within the json file.
+ * 
+ * The example.json file contains the path for the implementation of a feature
+ * graph builder.
  * 
  * @author Georgia Tech Trading Club Team #2
  * @since 1.0
--- app/src/main/java/gt/trading/openbook/featuregraph/features/package-info.java
+++ app/src/main/java/gt/trading/openbook/featuregraph/features/package-info.java
@@ -1,5 +1,15 @@
 /**
+ * This package contains basic features used within the feature graph.
  * 
+ * The BestAskFeature class is responsible for updating the best ask price
+ * through callbacks whenever a DepthEvent occurs.
+ * 
+ * The BestBidFeature class is responsible for updating the best bid price
+ * through callbacks whenever a DepthEvent occurs.
+ * 
+ * The MidPriceFeature class is responsible for updating the midprice and
+ * is a child feature of the BestAskFeature and BestBidFeature. It is updated
+ * through callbacks whenever the BestAsk or BestBid is updated.
  * 
  * @author Georgia Tech Trading Club Team #2
  * @since 1.0
